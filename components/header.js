e431a87624cdb226b9b67201783f2a8dcc361846
“taketo45” <taketo45@gmail.com>
2025-01-18 16:29:23 +0900

    ♻️ ヘッダー部分のProps化

--- components/header.js
+++ components/header.js
@@ -1,14 +1,19 @@
 import Image from "next/image";
 
-export function Header() {
+export function Header(props) {
+  console.log(props);
   return (
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <div>
+          <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+          <h1 className="text-2xl sm:text-3xl font-bold text-center sm:text-left">{props.title} : {props.infra} 2025/01</h1>
+        </div>
+        
   );
 }
