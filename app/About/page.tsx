2ed93fb1d356a40518f584edf68971861a73e3e3
“taketo45” <taketo45@gmail.com>
2025-01-19 15:11:33 +0900

    ✨ useState,Linkの導入

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,28 +1,20 @@
 'use client';
-import {Footer} from "../components/footer";
-import {Main} from "../components/main";
-import {Header} from "../components/header";
-import {useState} from "react";
+import {Footer} from "../../components/footer";
+import {Main} from "../../components/main";
+import {Header} from "../../components/header";
 
-export default function Home() {
+export default function About() {
 
   const alertFunc = () => {
     alert("クリック")
   };
-
-  const [count, setCount] = useState(0);
-  const countHandler = () => {
-    setCount((count) => count + 1);
-  };
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-      <Header title="Start Page" date={Date()} >
+      <Header title="About Page" date={Date()} >
         My first Next.jsテスト環境
       </Header>
-      <div>{count}</div>
-      <button onClick={countHandler}>クリック</button>
+
       <Main 
         no1="Get started by editing" 
         no1dir="app/page.tsx" 
--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export function Header(props) {
   // console.log(props);
@@ -13,6 +14,20 @@ export function Header(props) {
             priority
           />
           <h1 className="text-2xl sm:text-3xl font-bold text-center sm:text-left">{props.title} : {props.children} {props.date}</h1>
+          <nav>
+            <ul className="flex justify-center sm:justify-start space-x-4">
+              <li>
+                <Link href="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href="/About">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
         
   );
